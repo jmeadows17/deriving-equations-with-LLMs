d95b787c818052884461c4665eb902abf286b4b3
jmeadows17 <J.C.Meadows@hotmail.com>
2023-02-28 15:22:42 +0000

    Add files via upload

---
+++ translation_training.py
@@ -0,0 +1,132 @@
+import json
+import torch
+import numpy as np
+from tqdm import tqdm
+import evaluate
+from datasets import load_dataset, Dataset, concatenate_datasets
+from transformers import AutoTokenizer, AutoModelForSeq2SeqLM, DataCollatorForSeq2Seq, Seq2SeqTrainer, Seq2SeqTrainingArguments
+import os
+
+device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
+
+with open("translations.json", "r") as f:
+    d_json = json.load(f)
+
+dataset = Dataset.from_list(d_json)
+dataset = dataset.train_test_split(test_size=0.05)
+
+model_id="google/flan-t5-base"
+
+# Load tokenizer of FLAN-t5-base
+tokenizer = AutoTokenizer.from_pretrained(model_id)
+
+# The maximum total input sequence length after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded.
+tokenized_inputs = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["prompt"], truncation=True), batched=True, remove_columns=["prompt", "output"])
+max_source_length = max([len(x) for x in tokenized_inputs["input_ids"]])
+print(f"Max source length: {max_source_length}")
+
+# The maximum total sequence length for target text after tokenization.
+# Sequences longer than this will be truncated, sequences shorter will be padded."
+tokenized_targets = concatenate_datasets([dataset["train"], dataset["test"]]).map(lambda x: tokenizer(x["output"], truncation=True), batched=True, remove_columns=["prompt", "output"])
+max_target_length = max([len(x) for x in tokenized_targets["input_ids"]])
+print(f"Max target length: {max_target_length}")
+
+def preprocess_function(sample,padding="max_length"):
+    # add prefix to the input for t5
+    inputs = sample["prompt"]
+
+    # tokenize inputs
+    model_inputs = tokenizer(inputs, max_length=max_source_length, padding=padding, truncation=True)
+
+    # Tokenize targets with the `text` keyword argument
+    labels = tokenizer(text=sample["output"], max_length=max_target_length, padding=padding, truncation=True)
+
+    # If we are padding here, replace all tokenizer.pad_token_id in the labels by -100 when we want to ignore
+    # padding in the loss.
+    if padding == "max_length":
+        labels["input_ids"] = [
+            [(l if l != tokenizer.pad_token_id else -100) for l in label] for label in labels["input_ids"]
+        ]
+
+    model_inputs["labels"] = labels["input_ids"]
+    return model_inputs
+
+tokenized_dataset = dataset.map(preprocess_function, batched=True, remove_columns=["prompt", "output"])
+print(f"Keys of tokenized dataset: {list(tokenized_dataset['train'].features)}")
+
+# huggingface hub model id
+model_id="google/flan-t5-base"
+
+# load model from the hub
+model = AutoModelForSeq2SeqLM.from_pretrained(model_id).to(device)
+
+
+# Metric
+metric = evaluate.load("exact_match")
+
+# # helper function to postprocess text
+# def postprocess_text(preds, labels):
+
+#     preds = [pred + " \n" for pred in preds]
+#     labels = [label + " \n" for label in labels]
+
+#     return preds, labels
+
+def compute_metrics(eval_preds):
+    preds, labels = eval_preds
+    if isinstance(preds, tuple):
+        preds = preds[0]
+    decoded_preds = tokenizer.batch_decode(preds, skip_special_tokens=True)
+    # Replace -100 in the labels as we can't decode them.
+    labels = np.where(labels != -100, labels, tokenizer.pad_token_id)
+    decoded_labels = tokenizer.batch_decode(labels, skip_special_tokens=True)
+
+    result = metric.compute(predictions=decoded_preds, references=decoded_labels, use_stemmer=True)
+    result = {k: round(v * 100, 4) for k, v in result.items()}
+    prediction_lens = [np.count_nonzero(pred != tokenizer.pad_token_id) for pred in preds]
+    result["gen_len"] = np.mean(prediction_lens)
+    return result
+
+# we want to ignore tokenizer pad token in the loss
+label_pad_token_id = -100
+# Data collator
+data_collator = DataCollatorForSeq2Seq(
+    tokenizer,
+    model=model,
+    label_pad_token_id=label_pad_token_id,
+    pad_to_multiple_of=8
+)
+
+OUTPUT_DIR = os.getcwd() + "\models"
+
+# Define training args
+training_args = Seq2SeqTrainingArguments(
+    eval_steps = 500,
+    save_steps = 1000,
+    output_dir=OUTPUT_DIR,
+    gradient_accumulation_steps = 4,
+    per_device_train_batch_size=2,
+    per_device_eval_batch_size=2,
+    predict_with_generate=True,
+    fp16=False, # Overflows with fp16
+    learning_rate=5e-5,
+    num_train_epochs=5,
+    # logging & evaluation strategies
+    evaluation_strategy="steps",
+    save_strategy="steps",
+    save_total_limit=2,
+    load_best_model_at_end=True,
+    # metric_for_best_model="overall_f1",
+)
+
+# Create Trainer instance
+trainer = Seq2SeqTrainer(
+    model=model,
+    args=training_args,
+    train_dataset=tokenized_dataset["train"],
+    eval_dataset=tokenized_dataset["test"],
+    compute_metrics=compute_metrics,
+)
+
+trainer.train()
